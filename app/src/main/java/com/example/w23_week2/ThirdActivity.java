67f9013188017cc6b163d4dc8d76f43b48da75ef
rania <rania.arbash@senecacollege.ca>
2023-02-01 16:18:12 -0500

    ConstraintLayout

---
+++ app/src/main/java/com/example/w23_week2/ThirdActivity.java
@@ -0,0 +1,13 @@
+package com.example.w23_week2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
